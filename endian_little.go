a938fb150d47536d63d6bb2f0c4433091cb8c223
Matt Layher <mdlayher@gmail.com>
2022-02-21 14:42:59 -0500

    native: add loong64 to little endian architectures
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- endian_little.go
+++ endian_little.go
@@ -1,5 +1,5 @@
-//go:build amd64 || 386 || arm || arm64 || mipsle || mips64le || ppc64le || riscv64 || wasm
-// +build amd64 386 arm arm64 mipsle mips64le ppc64le riscv64 wasm
+//go:build amd64 || 386 || arm || arm64 || loong64 || mipsle || mips64le || ppc64le || riscv64 || wasm
+// +build amd64 386 arm arm64 loong64 mipsle mips64le ppc64le riscv64 wasm
 
 package native
 
