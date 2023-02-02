6c07abdb980e163c70522832bcb489690f9fe8fa
Brad Fitzpatrick <bradfitz@golang.org>
2023-02-01 20:21:28 -0800

    Use golang.org/x/sys/cpu.IsBigEndian.
    See golang/go#57237 for history.

--- endian_generic.go
+++ endian_generic.go
@@ -1,31 +1,22 @@
-//go:build !mips && !mips64 && !ppc64 && !s390x && !amd64 && !386 && !arm && !arm64 && !loong64 && !mipsle && !mips64le && !ppc64le && !riscv64 && !wasm
-// +build !mips,!mips64,!ppc64,!s390x,!amd64,!386,!arm,!arm64,!loong64,!mipsle,!mips64le,!ppc64le,!riscv64,!wasm
-
-// This file is a fallback, so that package native doesn't break
-// the instant the Go project adds support for a new architecture.
-//
-
 package native
 
 import (
 	"encoding/binary"
-	"log"
-	"runtime"
-	"unsafe"
+
+	"golang.org/x/sys/cpu"
 )
 
 var Endian binary.ByteOrder
 
-var IsBigEndian bool
+// IsBigEndian records whether the GOARCH's byte order is big endian.
+//
+// Deprecated: use golang.org/x/sys/cpu.IsBigEndian instead, now that it exists.
+const IsBigEndian = cpu.IsBigEndian
 
 func init() {
-	b := uint16(0xff) // one byte
-	if *(*byte)(unsafe.Pointer(&b)) == 0 {
+	if IsBigEndian {
 		Endian = binary.BigEndian
-		IsBigEndian = true
 	} else {
 		Endian = binary.LittleEndian
-		IsBigEndian = false
 	}
-	log.Printf("github.com/josharian/native: unrecognized arch %v (%v), please file an issue", runtime.GOARCH, Endian)
 }
